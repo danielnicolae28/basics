7a38c399cc00cec4af418e5b16cdf131d5f5868b
danielnicolae28 <danielnicolae286@gmail.com>
2025-03-09 17:08:44 +0200

    structs 2

--- structs/structs.go
+++ structs/structs.go
@@ -1,6 +1,7 @@
 package structs
 
 import (
+	"errors"
 	"fmt"
 )
 
@@ -10,19 +11,63 @@ type User struct {
 	Birthdate string
 }
 
+type Admin struct {
+	email    string
+	password string
+	User     // structs embeding
+}
+
 func (u *User) clearUserName() {
 	u.FirstName = ""
 	u.LastName = ""
 }
 
+func NewAdmin(email, password string) *Admin {
+
+	return &Admin{
+		email:    email,
+		password: password,
+		User: User{
+			FirstName: "Admin",
+			LastName:  "admin",
+			Birthdate: "---",
+		},
+	}
+
+}
+
+// constructor function
+func NewUser(firstName, lastName, birthday string) (*User, error) {
+
+	if firstName == "" || lastName == "" || birthday == "" {
+		return nil, errors.New("empty input") // check the input
+	}
+
+	return &User{
+		FirstName: firstName, // capitalize to be used in other package
+		LastName:  lastName,
+		Birthdate: birthday,
+	}, nil
+}
+
 func Structs() {
 
-	// var appUser User
+	var appUser *User
+	var appAdmin *Admin
 
-	appUser := User{
-		FirstName: getUserData("Please enter the first name: "),
-		LastName:  getUserData("Please enter the last name: "),
-		Birthdate: getUserData("Please enter the birthdate (dd/mm/yyyy): "),
+	firstName := getUserData("Please enter the first name: ")
+	lastName := getUserData("Please enter the last name: ")
+	birthday := getUserData("Please enter the birthdate (dd/mm/yyyy): ")
+	email := getUserData("Please enter the email: ")
+	password := getUserData("Please enter the pass ")
+
+	appUser, err := NewUser(firstName, lastName, birthday)
+	appAdmin = NewAdmin(email, password)
+
+	if err != nil {
+		fmt.Println("error")
+		return
+
 	}
 
 	// outputUserDetails(appUser) ///
@@ -30,6 +75,7 @@ func Structs() {
 	appUser.outputUserDetails() // calling the method
 	appUser.clearUserName()
 	appUser.outputUserDetails()
+	appAdmin.outputAdmin()
 }
 
 // passing structs as arguments
@@ -48,6 +94,10 @@ func (u User) outputUserDetails() {
 
 	fmt.Println(u.FirstName, u.LastName, u.Birthdate)
 
+}
+
+func (a Admin) outputAdmin() {
+	fmt.Println(a.email, a.User.LastName, a.password)
 }
 
 func getUserData(promptText string) string {
