1c689e963ded90321036dccb2a933593d4e7b2c5
danielnicolae28 <danielnicolae286@gmail.com>
2025-03-23 08:15:34 +0200

    functions

--- functions/functions.go
+++ functions/functions.go
@@ -2,11 +2,14 @@ package functions
 
 import "fmt"
 
+// custom function type
+// type anotFunc func(int, []string, map[string][]int) ([]int, string)
+
 func Functions() {
 	fmt.Println("Functions")
 
 	numbers := []int{1, 2, 3, 4}
-	doubled := doubleNumbers(&numbers)
+	doubled := transformNums(&numbers, double)
 
 	fmt.Println(doubled)
 
@@ -23,4 +26,15 @@ func doubleNumbers(numbers *[]int) []int {
 
 func double(num int) int {
 	return num * 2
+}
+
+// function as funtion parameters
+
+func transformNums(numbers *[]int, transform func(int) int) []int {
+	dNumbers := []int{}
+	for _, val := range *numbers {
+		dNumbers = append(dNumbers, transform(val))
+	}
+
+	return dNumbers
 }
